3855a87744c477fb9a18f5fbd64e1861681c99d6
Piyush Suthar <114796374+PiyushSuth@users.noreply.github.com>
2023-04-30 17:22:21 +0530

    Update rock paper scissors.py

--- rock paper scissors.py
+++ rock paper scissors.py
@@ -2,15 +2,9 @@ import random
 
 moves = "Rock", "Paper", "Scissors"
 
-player_moves = int(input("Chose Rock as 1, Paper as 2 and Scissors as 3:"))
+player_moves = int(input("Choose Rock as 1, Paper as 2 and Scissors as 3:"))
 comp_moves = random.choice(moves)
 print(f"Computer has chosen {comp_moves}")
-# if player_moves == 1:
-#     print("You have chosen rock")
-# elif player_moves == 2:
-#     print("You have chosen paper")
-# elif player_moves == 3:
-#     print("You have chosen rock")
 
 if player_moves == comp_moves:
     print("Its a tie")
